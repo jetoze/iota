6334d88f19e1b1c46e29edca1a22f89040067ebe
Torgil Zethson <tzethson@salesforce.com>
2017-07-03 16:55:34 -0700

    Enable anti-aliasing when drawing the cards.

--- src/main/java/jetoze/iota/ui/CardUi.java
+++ src/main/java/jetoze/iota/ui/CardUi.java
@@ -7,8 +7,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.RenderingHints;
 
 import javax.swing.JComponent;
 
@@ -39,7 +38,9 @@ public class CardUi extends JComponent {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g.create();
-		// Border:
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Border:
 		g2.setColor(UiConstants.CARD_BORDER_COLOR);
 		g2.drawRoundRect(0, 0, getWidth(), getHeight(), 6, 6);
 
